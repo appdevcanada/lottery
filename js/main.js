2dcaa9a331bb20f18f78a0213ddbbd9084101d3b
Luis Henrique Souza <mora0199@algonquinlive.com>
2018-11-06 12:31:00 -0500

    final version

--- js/main.js
+++ js/main.js
@@ -14,37 +14,44 @@ function getData() {
     let initDigits = document.getElementById("digits").value;
     let maxNumber = document.getElementById("max").value;
 
+    // to try if both numbers are filled
     if (initDigits.length > 0 && maxNumber.length > 0) {
         if (Number(maxNumber) >= Number(initDigits)) {
-            document.getElementById("home").style.display = "none";
-            document.getElementById("list").style.display = "block";
-            let paramVars = "digits=" + initDigits + "&max=" + maxNumber
-            let opts = {
-                method: 'POST',
-                mode: 'cors'
-            };
-            fetch(mainURL + paramVars, opts)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-
-                    if (data.code == 0) {
-                        let ul = document.querySelector(".num_list");
-                        ul.innerHTML = "";
-                        for (let item in data.numbers) {
-                            let li = document.createElement("li");
-                            li.innerHTML = data.numbers[item];
-                            ul.appendChild(li);
+            // to avoid code 534 as return, but no ERROR raised 
+            if (Number(initDigits) > 0 && Number(initDigits) < 11) {
+                document.getElementById("home").style.display = "none";
+                document.getElementById("list").style.display = "block";
+                let paramVars = "digits=" + initDigits + "&max=" + maxNumber
+                let opts = {
+                    method: 'POST',
+                    mode: 'cors'
+                };
+                fetch(mainURL + paramVars, opts)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        if (data.code == 0) {
+                            let ul = document.querySelector(".num_list");
+                            ul.innerHTML = "";
+                            for (let item in data.numbers) {
+                                let li = document.createElement("li");
+                                li.innerHTML = data.numbers[item];
+                                ul.appendChild(li);
+                            }
                         }
-                    }
-                })
-                .then(function (back) {
-                    document.getElementById("btnBack").addEventListener("click", resetDoc);
-                })
-                .catch(function (error) {
-                    document.getElementById("errorMessage").textContent = "Error: " + error.message;
-                })
+                    })
+                    .then(function (back) {
+                        document.getElementById("btnBack").addEventListener("click", resetDoc);
+                    })
+                    .catch(function (error) {
+                        document.getElementById("errorMessage").textContent = "Error: " + error.message;
+                        document.getElementById("btnBack").addEventListener("click", resetDoc);
+                    })
+            } else {
+                alert("You have to enter a valid number \nbetween 1 and 10");
+                document.getElementById("digits").focus();
+            }
         } else {
             alert("You have to enter a valid number \ngreater than or equal to number of Digits");
             document.getElementById("max").focus();
@@ -60,6 +67,8 @@ function getData() {
 }
 
 function resetDoc() {
+    let ul = document.querySelector(".num_list");
+    ul.innerHTML = "";
     document.getElementById("home").style.display = "block";
     document.getElementById("list").style.display = "none";
     document.getElementById("digits").value = "";
